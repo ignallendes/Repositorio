105a2772c828eed2c9e7de56bfe89c81578ce450
ignallendes <igna.allendes@gmail.com>
2022-04-25 22:34:33 -0400

    Commit

--- Hola mundo.py
+++
@@ -1,3 +0,0 @@
-print("Hola")
-print("Mundo")
-print("Adios")
---
+++ manipulacion_cadenas/manipulacion_cadenas_de_texto.py
@@ -0,0 +1,15 @@
+#El operador "*" se puede emplear para repetir una cadena de texto miltiplicandola
+print("abc"*3)
+#Los string pueden verse como listas de caracteres
+a = "Hola Mundo"
+print(a[0])
+print(a[0:5])
+print(a[-6:-1])
+print("Mundo" in a)
+print("Adios" in a)
+#Operadores comunes para alterar cadenas(Empleando variable "a")
+#a.lower() Toda la cadena pasa a minusculas
+#a.upper() Toda la cadena pasa a mayusculas
+#a.capitalize() Primera letra de la cadena en mayusculas
+#a.title() Primera letra de cada palanra de la cadena en mayus
+#a.swapcase() Los mayus y minus de la cadena se invierten
