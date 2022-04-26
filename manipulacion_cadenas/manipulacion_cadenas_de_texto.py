9d6e26bc3c827f688d2fa89815e9075f81332857
ignallendes <igna.allendes@gmail.com>
2022-04-25 23:06:06 -0400

    Commit

--- manipulacion_cadenas/manipulacion_cadenas_de_texto.py
+++ manipulacion_cadenas/manipulacion_cadenas_de_texto.py
@@ -13,3 +13,22 @@ print("Adios" in a)
 #a.capitalize() Primera letra de la cadena en mayusculas
 #a.title() Primera letra de cada palanra de la cadena en mayus
 #a.swapcase() Los mayus y minus de la cadena se invierten
+#len(a) Permite contar los caracteres de la cadena de texto
+
+#Operadores que permiten limpiar una cadena
+b = "  Hola Mundo Adios "
+print(b)
+print(b.strip())# Elimina espacios al principio y al final
+print(b.rjust(22))# Alinia a la derecha hasta alcancar la longuitud ingresada(en ese caso 22)
+print(b.ljust(22))# Alinia a la izq hasta alcancar la longuitud ingresada //
+print(b.center(22))#Centra la cadena hasta alcanzar la longuitid ingresada
+
+#Podemos partir una cadena empleando "split"
+c = "Hola_mundo_adios"
+print(c.split("_"))
+#Podemos unir los elementos de una lista en una cadena, empleando una subcadena de "espaciado"
+d = ["Hola","Mundo","Que tal?"]
+print("_".join(d))
+print(" ".join(d))
+
+
